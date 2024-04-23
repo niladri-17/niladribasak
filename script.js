b5a06bc556649ed7a8ea1e7156f455a32a170c9a
Niladri Basak <niladri.basak.007@gmail.com>
2024-04-24 02:12:11 +0530

    modified:   index.html 	modified:   script.js 	modified:   style.css 	modified:   success.html

--- script.js
+++ script.js
@@ -4,6 +4,91 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
+var hoverMouse = function ($el) {
+  $el.each(function () {
+    var $self = $(this);
+    var hover = false;
+    var offsetHoverMax = $self.attr("offset-hover-max") || 0.7;
+    var offsetHoverMin = $self.attr("offset-hover-min") || 0.5;
+
+    var attachEventsListener = function () {
+      $(window).on("mousemove", function (e) {
+        //
+        var hoverArea = hover ? offsetHoverMax : offsetHoverMin;
+
+        // cursor
+        var cursor = {
+          x: e.clientX,
+          y: e.clientY - $(window).scrollTop(),
+        };
+
+        // size
+        var width = $self.outerWidth();
+        var height = $self.outerHeight();
+
+        // position
+        var offset = $self.offset();
+        var elPos = {
+          x: offset.left + width / 2,
+          y: offset.top + height / 2,
+        };
+
+        // comparaison
+        var x = cursor.x - elPos.x;
+        var y = cursor.y - elPos.y;
+
+        // dist
+        var dist = Math.sqrt(x * x + y * y);
+
+        // mutex hover
+        var mutHover = false;
+
+        // anim
+        if (dist < width * hoverArea) {
+          mutHover = true;
+          if (!hover) {
+            hover = true;
+          }
+          onHover(x, y);
+        }
+
+        // reset
+        if (!mutHover && hover) {
+          onLeave();
+          hover = false;
+        }
+      });
+    };
+
+    var onHover = function (x, y) {
+      gsap.to($self, 0.4, {
+        x: x * 0.8,
+        y: y * 0.8,
+        //scale: .9,
+        rotation: x * 0.05,
+        ease: Power2.easeOut,
+        // backgroundColor: red
+      });
+    };
+    var onLeave = function () {
+      gsap.to($self, 0.7, {
+        x: 0,
+        y: 0,
+        scale: 1,
+        rotation: 0,
+        // ease: Elastic.easeOut.config(1.2, 0.4),
+        ease: "elastic(1.2, 0.4)",
+      });
+    };
+
+    attachEventsListener();
+  });
+};
+
+// gsap.from(".circle",{
+//     backgroundColor
+// })
+
 function init() {
   gsap.registerPlugin(ScrollTrigger);
 
@@ -47,7 +132,7 @@ $(document).ready(function () {
 
   window.onbeforeunload = function () {
     this.scrollTo(0, 0);
-  }
+  };
 
   $(window).scroll(function () {
     // sticky navbar on scroll script
@@ -166,6 +251,7 @@ $(document).ready(function () {
   // Only invoke the function if isn't a touch device
   if (!isTouchDevice) {
     createCursorFollower();
+    hoverMouse($(".circle"));
   }
 
   gsap.from(".logo.welcome , a.menu-btn", {
@@ -185,19 +271,50 @@ $(document).ready(function () {
   });
 
   gsap.to(".scroll-up-btn", {
-    y: -60,
+    y: -100,
     repeat: -1,
     duration: 0.4,
     yoyo: true,
+    ease: "power4.out"
   });
 
+  const circleAnim = gsap.timeline({ repeat: -1 });
+  circleAnim.to(".circle", {
+    y: `-2rem`,
+    duration: 0.5,
+    yoyo: true,
+  });
+
+  circleAnim.to(".circle", {
+    y: 0,
+    duration: 0.3,
+    yoyo: true,
+  });
+
+  function pauseAnimation() {
+    circleAnim.pause();
+}
+
+  function resumeAnimation() {
+    circleAnim.resume();
+}
+
+if (!isTouchDevice){
+  document
+    .querySelector(".circle")
+    .addEventListener("mouseenter", pauseAnimation);
+
+  document
+    .querySelector(".circle")
+    .addEventListener("mouseleave", resumeAnimation);
+}
   // navHeight : '70.8px';
   let tl1 = gsap.timeline({
     scrollTrigger: {
       trigger: "h1",
       start: "bottom 120",
       end: "bottom 70.8",
-      markers: true,
+      //  markers: true,
       scrub: 1,
     },
   });
